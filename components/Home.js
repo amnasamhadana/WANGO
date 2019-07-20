c8ff8be425ebb95f14e6022156ca6780b58ecec1
amnasamhadana <abosamhadanaamna@gmail.com>
2019-07-20 17:39:58 +0300

    lol

--- components/Home.js
+++ components/Home.js
@@ -10,37 +10,87 @@ import {
   TextInput,
   FlatList,
   Button,
-  Picker
+  Picker,
+  ImageBackground
 } from "react-native";
 
 export default class Home extends React.Component {
-  back = () => {
-    this.props.navigation.navigate("login");
-  };
   render() {
     return (
       //<ScrollView cotentContainerStyle ={styles.container}>
-      <ScrollView
-        contentContainerStyle={{
-          alignItems: "center",
-          marginRight: 350,
-          back
-        }}
-      >
-        <TouchableOpacity onPress={this.back}>
-          <Image
-            style={{ width: 40, height: 40, marginTop: 30 }}
-            source={require("../assets/back.png")}
+
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../assets/background.png")}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <Text>Inside</Text>
+
+          <FlatList
+            style={styles.list}
+            numColumns={2}
+            data={people}
+            keyExtractor={(item, index) => item.caption}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={{ flex: 0.5 }}
+                onPress={() => this.handlePress(item)}
+              >
+                <Image style={styles.img1} source={item.image} />
+              </TouchableOpacity>
+            )}
           />
-        </TouchableOpacity>
-      </ScrollView>
+        </ImageBackground>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   img1: {
-    height: 200,
-    width: 200
+    height: 100,
+    width: 100,
+    margin: 10
+  },
+  list: {
+    backgroundColor: "black",
+    marginLeft: "10%",
+    height: "80%",
+    padding: "10%",
+    marginTop: "10%",
+    marginBottom: "10%",
+    borderRadius: 14,
+    width: "80%"
   }
 });
+
+const people = [
+  {
+    name: "logo",
+    caption: " logo",
+    image: require("../assets/time.png")
+  },
+  {
+    name: "chat",
+    caption: "chat",
+    image: require("../assets/wish.png")
+  },
+  {
+    name: "time",
+    caption: "time",
+    image: require("../assets/food.png")
+  },
+  {
+    name: "WishList",
+    caption: " WishList",
+    image: require("../assets/doctor.png")
+  },
+  {
+    name: "food",
+    caption: " food",
+    image: require("../assets/health.png")
+  }
+];
